57e1af25eee39330591a87e44db68dc64dfe8cc2
lydiaterki <lydiaterki@icloud.com>
2025-01-08 11:19:27 +0100

    fix bug gitignore

--- src/main/java/fr/efrei/springrag/web/SampleResource.java
+++ src/main/java/fr/efrei/springrag/web/SampleResource.java
@@ -1,4 +1,18 @@
 package fr.efrei.springrag.web;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class SampleResource {
+
+    @GetMapping("/samples/{value}")
+    public String hello(@PathVariable(value = "value", required = false) final String value) {
+        return "Hello " + value + "!";
+    }
+
+    /* @GetMapping("/samples/dto/{value}")
+    public Sample helloJson(@PathVariable(value = "value", required = false) final String value) {
+        String s1 = "Hello " + value + "!";
+        return new Sample(s1);
+    } */
 }
